0afb16069bb5dc2e36dbaabb8a7ca75d8d6333b3
Rimjhim <rimjhim@Rimjhims-MacBook-Air.local>
2026-06-25 20:54:11 +0530

    week 2 - SpringCore_Maven

---
+++ Week2/LibraryManagement/src/main/java/com/library/aspect/LoggingAspect.java
@@ -0,0 +1,20 @@
+package com.example.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* com.example.service.*.*(..))")
+    public void logBeforeMethod(JoinPoint joinPoint) {
+
+        System.out.println(
+            "Method Called: "
+            + joinPoint.getSignature().getName()
+        );
+    }
+}
