63e1e3f8f6dba3e41ffdd52516ef468e9c0b8e51
Rimjhim <rimjhim@Rimjhims-MacBook-Air.local>
2026-07-16 17:26:33 +0530

    implement data binding in home component

--- Week5/Angular/Beginner/Hands-On_1/student-course-portal/src/app/pages/home/home.ts
+++ Week5/Angular/Beginner/Hands-On_1/student-course-portal/src/app/pages/home/home.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule , NgIf],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home {
+
+  // Interpolation
+  portalName = 'Student Course Portal';
+
+  // Property Binding
+  isPortalActive = true;
+
+  // Event Binding
+  message = '';
+
+  // Two-way Binding
+  searchTerm = '';
+
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
+}
